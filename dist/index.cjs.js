d18b0fdbe6b92e3c4f5d7d8a05c442b377ebfcc2
Mosqueiro <erickcosta2010@gmail.com>
2021-03-26 03:56:39 -0300

    new colors

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -3253,22 +3253,22 @@ var templateObject_1$P;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#1FC7D4",
-    primaryBright: "#53DEE9",
-    primaryDark: "#0098A1",
-    secondary: "#7645D9",
+    primary: "#0f5f54",
+    primaryBright: "#0f5f54",
+    primaryDark: "#0f5f54",
+    secondary: "#0e3731",
     success: "#31D0AA",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", inputSecondary: "#d7caec", tertiary: "#EFF4F5", text: "#452A7A", textDisabled: "#BDC2C4", textSubtle: "#8f80ba", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f1f0f0", backgroundDisabled: "#E9EAEB", contrast: "#0b2b27", invertedContrast: "#FFFFFF", input: "#ebf4ea", tertiary: "#EFF4F5", text: "#0e3731", textDisabled: "#BDC2C4", textSubtle: "#1e5951", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
-    } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#9A6AFF", background: "#100C18", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", inputSecondary: "#66578D", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#A28BD4", borderColor: "#524B63", card: "#27262c", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
-    } });
+    }, inputSecondary: "#caecd1" });
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#0f5f54", background: "#343135", backgroundDisabled: "#28a392", contrast: "#FFFFFF", invertedContrast: "#0d4039", input: "#0098A1", primaryDark: "#0098A1", tertiary: "#28a392", text: "#FFFFFF", textDisabled: "#cccccc", textSubtle: "#c9c4d4", borderColor: "#28a392", card: "#27262c", gradients: {
+        bubblegum: "linear-gradient(139.73deg, #0f5f54 0%, #0e3731 100%)",
+    }, inputSecondary: "#5b8d57" });
 
 var light = {
     background: lightColors.card,
@@ -3312,11 +3312,11 @@ var dark$3 = {
 
 var light$4 = {
     background: lightColors.card,
-    hover: "#EEEAF4",
+    hover: "#ebf4ea",
 };
 var dark$4 = {
     background: darkColors.card,
-    hover: "#473d5d",
+    hover: "#3d5d3f",
 };
 
 var light$5 = {
