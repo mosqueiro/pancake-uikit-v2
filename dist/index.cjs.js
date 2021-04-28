eaba5ca4cbac05ad9358fd4758b99092bbf3d879
Mosqueiro <erickcosta2010@gmail.com>
2021-04-28 02:50:20 -0300

    Teste Href

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2791,7 +2791,7 @@ var MenuLink = function (_a) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     var Tag = isHttpLink ? "a" : reactRouterDom.NavLink;
     var props = isHttpLink ? { href: href } : { to: href };
-    return React__default['default'].createElement(Tag, __assign({}, props, otherProps));
+    return React__default['default'].createElement(Tag, __assign({ target: "_blank" }, props, otherProps));
 };
 
 var Icons = IconModule;
@@ -2810,10 +2810,10 @@ var PanelBody = function (_a) {
             var initialOpenState = entry.initialOpenState === true ? entry.initialOpenState : itemsMatchIndex >= 0;
             return (React__default['default'].createElement(Accordion$1, { key: entry.label, isPushed: isPushed, pushNav: pushNav, icon: iconElement, label: entry.label, initialOpenState: initialOpenState, className: calloutClass, isActive: entry.items.some(function (item) { return item.href === location.pathname; }) }, isPushed &&
                 entry.items.map(function (item) { return (React__default['default'].createElement(MenuEntry, { key: item.href, secondary: true, isActive: item.href === location.pathname, onClick: handleClick },
-                    React__default['default'].createElement(MenuLink, { target: "_blank", href: item.href }, item.label))); })));
+                    React__default['default'].createElement(MenuLink, { href: item.href }, item.label))); })));
         }
         return (React__default['default'].createElement(MenuEntry, { key: entry.label, isActive: entry.href === location.pathname, className: calloutClass },
-            React__default['default'].createElement(MenuLink, { href: entry.href, target: "_blank", onClick: handleClick },
+            React__default['default'].createElement(MenuLink, { href: entry.href, onClick: handleClick },
                 iconElement,
                 React__default['default'].createElement(LinkLabelMemo, { isPushed: isPushed }, entry.label))));
     })));
--- src/widgets/Menu/components/PanelBody.tsx
+++ src/widgets/Menu/components/PanelBody.tsx
@@ -53,9 +53,7 @@ const PanelBody: React.FC<Props> = ({ isPushed, pushNav, isMobile, links }) => {
               {isPushed &&
                 entry.items.map((item) => (
                   <MenuEntry key={item.href} secondary isActive={item.href === location.pathname} onClick={handleClick}>
-                    <MenuLink target="_blank" href={item.href}>
-                      {item.label}
-                    </MenuLink>
+                    <MenuLink href={item.href}>{item.label}</MenuLink>
                   </MenuEntry>
                 ))}
             </Accordion>
@@ -63,7 +61,7 @@ const PanelBody: React.FC<Props> = ({ isPushed, pushNav, isMobile, links }) => {
         }
         return (
           <MenuEntry key={entry.label} isActive={entry.href === location.pathname} className={calloutClass}>
-            <MenuLink href={entry.href} target="_blank" onClick={handleClick}>
+            <MenuLink href={entry.href} onClick={handleClick}>
               {iconElement}
               <LinkLabel isPushed={isPushed}>{entry.label}</LinkLabel>
             </MenuLink>
