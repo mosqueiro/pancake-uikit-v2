eaf989db14803c2543b9abe7536ad55abea87523
Mosqueiro <erickcosta2010@gmail.com>
2021-05-12 01:34:02 -0300

    0.15.8

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -46,6 +46,44 @@ function __rest(s, e) {
     return t;
 }
 
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+function __generator(thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+}
+
 /** @deprecated */
 function __spreadArrays() {
     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
@@ -2853,11 +2891,38 @@ var templateObject_1$G;
 
 var PriceLink$1 = styled.a(templateObject_1$H || (templateObject_1$H = __makeTemplateObject(["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"], ["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"])));
 var CakePriceBoost = function (_a) {
+    // const newcakePriceUsd = 0;
     var cakePriceUsd = _a.cakePriceUsd;
-    var newcakePriceUsd = 0;
+    var api = 'https://finance.zcore.network/lp';
+    var _b = useState('0.000'), newcakePriceUsd = _b[0], setData = _b[1];
+    useEffect(function () {
+        var fetchData = function () { return __awaiter(void 0, void 0, void 0, function () {
+            var response, response_json, error_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 3, , 4]);
+                        return [4 /*yield*/, fetch(api)];
+                    case 1:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.json()];
+                    case 2:
+                        response_json = _a.sent();
+                        setData(response_json.lp_price);
+                        return [3 /*break*/, 4];
+                    case 3:
+                        error_1 = _a.sent();
+                        console.error('Unable to fetch price data:', error_1);
+                        return [3 /*break*/, 4];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        }); };
+        fetchData();
+    }, [setData]);
     return cakePriceUsd ? (React.createElement(PriceLink$1, { href: "https://zefi.zcore.network/ifo" },
         React.createElement(Icon$z, { width: "24px", mr: "8px" }),
-        React.createElement(Text, { color: "textSubtle", bold: true }, "$" + newcakePriceUsd.toFixed(3)))) : (React.createElement(Skeleton, { width: 80, height: 24 }));
+        React.createElement(Text, { color: "textSubtle", bold: true }, "$" + newcakePriceUsd))) : (React.createElement(Skeleton, { width: 80, height: 24 }));
 };
 var CakePriceBoost$1 = React.memo(CakePriceBoost);
 var templateObject_1$H;
--- src/widgets/Menu/components/CakePriceBoost.tsx
+++ src/widgets/Menu/components/CakePriceBoost.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useMemo, useState } from "react";
 import styled from "styled-components";
 import { PancakeRoundBoost } from "../../../components/Svg";
 import Text from "../../../components/Text/Text";
@@ -22,12 +23,29 @@ const PriceLink = styled.a`
 `;
 
 const CakePriceBoost: React.FC<Props> = ({ cakePriceUsd }) => {
-  const newcakePriceUsd = 0;
+  // const newcakePriceUsd = 0;
+
+  const api = "https://finance.zcore.network/lp";
+  const [newcakePriceUsd, setData] = useState("0.000");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(api);
+        const response_json = await response.json();
+        setData(response_json.lp_price);
+      } catch (error) {
+        console.error("Unable to fetch price data:", error);
+      }
+    };
+
+    fetchData();
+  }, [setData]);
 
   return cakePriceUsd ? (
     <PriceLink href="https://zefi.zcore.network/ifo">
       <PancakeRoundBoost width="24px" mr="8px" />
-      <Text color="textSubtle" bold>{`$${newcakePriceUsd.toFixed(3)}`}</Text>
+      <Text color="textSubtle" bold>{`$${newcakePriceUsd}`}</Text>
     </PriceLink>
   ) : (
     <Skeleton width={80} height={24} />
