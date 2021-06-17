c04f8f50135aa2b32e07a0984bc1e0db7bc64198
Mosqueiro <erickcosta2010@gmail.com>
2021-06-17 03:16:46 -0300

    0.37.3

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2865,16 +2865,16 @@ var additionalColors = {
     binance: "#F0B90B",
     overlay: "#0f5f54",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), additionalColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", backgroundAlt: "#FFFFFF", cardBorder: "#E7E3EB", contrast: "#191326", dropdown: "#F6F6F6", dropdownDeep: "#EEEEEE", invertedContrast: "#FFFFFF", input: "#eeeaf4", inputSecondary: "#caeccb", tertiary: "#EFF4F5", text: "#0e3731", textDisabled: "#BDC2C4", textSubtle: "#1e5951", disabled: "#E9EAEB", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #E5FDFF 0%, #F3EFFF 100%)",
-        inverseBubblegum: "linear-gradient(139.73deg, #F3EFFF 0%, #E5FDFF 100%)",
-        cardHeader: "linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)",
+var lightColors = __assign(__assign(__assign({}, baseColors), additionalColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", backgroundAlt: "#FFFFFF", cardBorder: "#E7E3EB", contrast: "#191326", dropdown: "#F6F6F6", dropdownDeep: "#EEEEEE", invertedContrast: "#FFFFFF", input: "#ebf4ea", inputSecondary: "#caeccb", tertiary: "#EFF4F5", text: "#0e3731", textDisabled: "#BDC2C4", textSubtle: "#1e5951", disabled: "#E9EAEB", gradients: {
+        bubblegum: "linear-gradient(139.73deg, #e9ffe5 0%, #f1ffef 100%)",
+        inverseBubblegum: "linear-gradient(139.73deg, #f2ffef 0%, #e5ffe6 100%)",
+        cardHeader: "linear-gradient(111.68deg, #ecf2ec 0%, #eaf6e8 100%)",
         blue: "linear-gradient(180deg, #b0f1a7 0%, #aff294 100%)",
         violet: "linear-gradient(180deg, #d4fbc9 0%, #c0fab8 100%)",
         violetAlt: "linear-gradient(180deg, #ceefcb 0%, #9bd09a 100%)",
         gold: "linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), additionalColors), { secondary: "#0f5f54", background: "#08060B", backgroundDisabled: "#28a392", backgroundAlt: "#27262c", cardBorder: "#334132", contrast: "#FFFFFF", dropdown: "#1E1D20", dropdownDeep: "#100C18", invertedContrast: "#191326", input: "#0098A1", inputSecondary: "#233021", primaryDark: "#0098A1", tertiary: "#353547", text: "#F4EEFF", textDisabled: "#637161", textSubtle: "#b2d2ad", disabled: "#4c634b", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), additionalColors), { secondary: "#0f5f54", background: "#08060B", backgroundDisabled: "#28a392", backgroundAlt: "#27262c", cardBorder: "#334132", contrast: "#FFFFFF", dropdown: "#1E1D20", dropdownDeep: "#100C18", invertedContrast: "#191326", input: "#0098A1", inputSecondary: "#233021", primaryDark: "#0098A1", tertiary: "#28a392", text: "#FFFFFF", textDisabled: "#cccccc", textSubtle: "#c9c4d4", disabled: "#4c634b", gradients: {
         bubblegum: "linear-gradient(139.73deg, #355c31 0%, #31542a 100%)",
         inverseBubblegum: "linear-gradient(139.73deg, #31542a 0%, #355c31 100%)",
         cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #304530 100%)",
